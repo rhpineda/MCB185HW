b6c6733e7554efdead3d9766ebbfffe1bc6172ac
rhpineda <rhpineda@ucdavis.edu>
2023-01-22 23:09:38 -0800

    25atseq.py

--- 25atseq.py
+++ 25atseq.py
@@ -8,6 +8,28 @@
 
 # Note: set random.seed() if you want repeatable random numbers
 
+#---------------------------------------------------------------------------
+import random
+dna = ''
+at = 0
+atfrac = 0
+
+for i in range(1,31):
+	r=random.randint(1,10)
+	if r == 1 or r == 2 or r == 3 :
+		dna += 'A'
+		at += 1
+	elif r == 4 or r == 5 or r == 6 :
+		dna += 'T'
+		at += 1
+	elif r == 7 or r == 8:
+		dna += 'G'
+	elif r == 9 or r ==10:
+		dna += 'C'		
+	atfrac = at/i
+	#print(atfrac) #Checking to see if AT fraction updates every loop
+print(len(dna), atfrac, dna)
+#---------------------------------------------------------------------------
 
 """
 python3 25atseq.py
