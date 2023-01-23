500dea32e5327925c6cdd33135db509d975f8f33
rhpineda <rhpineda@ucdavis.edu>
2023-01-22 22:28:43 -0800

    24gc.py

--- 24gc.py
+++ 24gc.py
@@ -3,9 +3,14 @@
 # Write a program that computes the GC% of a DNA sequence
 # Format the output for 2 decimal places
 
+#---------------------------------------------------------------------------
 dna = 'ACAGAGCCAGCAGATATACAGCAGATACTAT'
-
-
+gc = 0
+for i in range(len(dna) -1):
+	if (dna[i] == 'G' or dna[i] ==  'C'):
+		gc += 1
+print(f'{gc/(len(dna)):.2f}')
+#---------------------------------------------------------------------------
 """
 python3 24gc.py
 0.42
