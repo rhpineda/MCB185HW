362f94730d4ca5ad800ef7f512151e14d93c0f85
rhpineda <rhpineda@ucdavis.edu>
2023-01-29 01:33:16 -0800

    34listcomp.py

--- Unit3Tutorials/34listcomp.py
+++ Unit3Tutorials/34listcomp.py
@@ -8,36 +8,36 @@
 #    Is common in Python - so you must know it when you see it
 #    Isn't found in most languages, so consider it optional
 
-"""
+
 
 # Consider the following initialization code:
 
 data = [] # emtpy list to be filled with append()
 for i in range(10): data.append(0)
 print(data)
-
+print('#----------------------------------------------------------------------')
 # You can write this more succinctly with the * operator
 
 data = [0] * 10
 print(data)
-
+print('#----------------------------------------------------------------------')
 # Another alternative is list comprehension
 
 data = [0 for i in range(10)]
 print(data)
-
+print('#----------------------------------------------------------------------')
 # Here's a slightly more complex initialization
 
 squares = []
 for i in range(10):
 	squares.append(i ** 2)
 print(squares)
-
+print('#----------------------------------------------------------------------')
 # List comprehension turns 3 lines into 1
 
 squares = [i ** 2 for i in range(10)]
 print(squares)
-
+print('#----------------------------------------------------------------------')
 # You can also include a conditional
 # First the longer syntax
 
@@ -46,10 +46,10 @@ for i in range(10):
 	if i % 3 == 0:
 		square3.append(i ** 2)
 print(square3)
-
+print('#----------------------------------------------------------------------')
 # Now list comprehension
 
 square3 = [i ** 2 for i in range(10) if i % 3 == 0]
 print(square3)
-
+"""
 """
