2d881dbb5887c5ff43880501df0a6f4479ae3460
rhpineda <rhpineda@ucdavis.edu>
2023-01-29 01:23:45 -0800

    31tuples.py

--- Unit3Tutorials/31tuples.py
+++ Unit3Tutorials/31tuples.py
@@ -2,16 +2,16 @@
 
 # Move the triple quotes downward to uncover each segment of code
 
-"""
+
 
 # Sometimes we want to work with multiple variables at the same time
 # A convenient way to do this is a 'tuple'
 # Any time you have comma separated values, you have a 'tuple'
 # Tuples are often in parentheses but need not be
-
+print('#----------------------------------------------------------------------')
 tup = 1, 2, 3
 print(tup)      # all three values are contained in a single varible
-
+print('#----------------------------------------------------------------------')
 tup = (1, 2, 3)
 print(tup)      # same thing as above, parentheses optional
 
@@ -20,18 +20,18 @@ print(tup)      # same thing as above, parentheses optional
 #    parentheses around all of the values
 #    decimal point after the floating point value
 #    strings are in quotes
-
+print('#----------------------------------------------------------------------')
 tup = 1, 2.00, '3', 'four'
 print(tup)
 
 
 # You can use slice syntax, just like strings
-
+print('#----------------------------------------------------------------------')
 print(tup[1:])
 print(tup[:2])
 
 # Tuples are immutable, meaning you can't change their contents
-
+print('#----------------------------------------------------------------------')
 tup[1] = 'two'
 
-"""
+
