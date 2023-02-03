96ddb223bc8d3462eaeecf298a9cb261ccdf10c2
rhpineda <rhpineda@ucdavis.edu>
2023-02-03 04:07:48 -0800

    hella swag program

--- 32xcoverage.py
+++ 32xcoverage.py
@@ -14,7 +14,73 @@
 
 # Note: you will not get exactly the same results as the command line below
 
+#---------------------------------------------------------------------------
 
+import sys
+import random
+
+# input is : genome_length, read_length, num_read
+#Convert input to variables
+genome_length = int(sys.argv[1])
+read_length = int(sys.argv[2])
+num_read = int(sys.argv[3])
+
+#Genome list becomes list of nt of genome_length
+genome_list = []
+
+for i in range(genome_length): 
+	addnt = random.choice('ACGT')
+	genome_list.append(str(addnt))
+
+#nt coverage becomes list of coverage of given
+ntcoverage = []
+visualizecoverage = []
+for i in range(genome_length):
+	ntcoverage.append(0)
+	visualizecoverage.append('')
+for i in range(num_read): 
+	curreadstart = 0
+	curreadstart = random.randint(1, genome_length)
+	for j in range(read_length): 
+		if curreadstart + j > genome_length:
+			break
+		else:
+			ntcoverage[curreadstart+j-1] += 1
+			(visualizecoverage[curreadstart+j-1]) += '|'
+
+
+#Visualize covarage dist (column view)
+for a, b in zip(visualizecoverage, genome_list):
+	print (b, a)
+
+#Visualize covarage dist (row view)
+#print(ntcoverage)
+#print(genome_list)
+
+
+print('^^^^COVERAGE VISUALIZED^^^^')
+#Print min max, and raw avg
+print('EDGES INCLUDED')
+print('MIN',min(ntcoverage), \
+	'MAX', max(ntcoverage), \
+	'AVG', (sum(ntcoverage)/genome_length))
+
+#Accounting for edge effects
+firstedgelessnt	= ntcoverage[read_length:]
+
+print('FIRST EDGE NOT INCLUDED')
+print('MIN', min(ntcoverage), \
+	'MAX',max(ntcoverage),\
+	'AVERAGE', (sum(firstedgelessnt)/((genome_length)-(read_length))))
+
+#Accounting for edge effects
+edgelessnt	= ntcoverage[read_length:genome_length-read_length]
+
+print('BOTH EDGES NOT INCLUDED')
+print('MIN', min(ntcoverage), \
+	'MAX',max(ntcoverage),\
+	'AVERAGE', (sum(edgelessnt)/((genome_length)-(2*read_length))))
+#---------------------------------------------------------------------------
 """
 python3 32xcoverage.py 1000 100 100
 5 20 10.82375
